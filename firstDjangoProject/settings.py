c19bbe3e9aafd2d0af8c8f36261736eede9a78d9
DmitrovicAleksa <aleksa.dmitrovic@vivifyideas.com>
2021-06-21 15:05:22 +0200

    Izvrsene promene

--- firstDjangoProject/settings.py
+++ firstDjangoProject/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'application.apps.ApplicationConfig',
+    'application.apps.TodosConfig',
     # base
     'django.contrib.admin',
     'django.contrib.auth',
