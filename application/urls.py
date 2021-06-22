a24624d742a632a882ffacb987823c201e9b3df4
DmitrovicAleksa <aleksa.dmitrovic@vivifyideas.com>
2021-06-22 17:03:37 +0200

    Serializers done

--- application/urls.py
+++ application/urls.py
@@ -1,8 +1,17 @@
-from django.urls import path
+from django.conf.urls import url
+from django.urls import path, include
 from . import views
+from rest_framework.routers import DefaultRouter
+
+
+router = DefaultRouter()
+router.register(r'users', views.UsersViewSet, basename="users")
+
 
 app_name = 'todos'
 
 urlpatterns = [
-   path('todos/',views.HomeView.as_view())
+    path('todos/', views.HomeView.as_view()),
+    path('task-list/', views.HomeView.as_view()),
+    url('', include(router.urls))
 ]
--- firstDjangoProject/settings.py
+++ firstDjangoProject/settings.py
@@ -78,8 +78,10 @@ WSGI_APPLICATION = 'firstDjangoProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'application',  # database name
+        'USER': 'root',  # database username
+        'PASSWORD': 'root',  # database password
     }
 }
 
