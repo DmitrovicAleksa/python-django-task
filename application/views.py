4b3ea379d9b493cfc12ead5d4ae7ff8da413daea
DmitrovicAleksa <aleksa.dmitrovic@vivifyideas.com>
2021-06-25 17:34:58 +0200

    Deleted imports

--- application/views.py
+++ application/views.py
@@ -1,15 +1,9 @@
-from re import search
-from django.db.models import query
-from django.http import request
-from rest_framework import views
-import rest_framework
-from rest_framework.generics import ListAPIView
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import ToDo, User
 from .serializers import UserSerializer
-from rest_framework import mixins, viewsets
-from rest_framework import authentication, permissions
+from rest_framework import viewsets
+from rest_framework import permissions
 from .permissions import AdminAccessPermission
 from rest_framework.filters import SearchFilter
 
@@ -20,7 +14,6 @@ class HomeView(APIView):
         return Response({"todos": todos})
 
 
-
 class UsersViewSet(viewsets.ModelViewSet):
 
     queryset = User.objects.all()
@@ -29,7 +22,6 @@ class UsersViewSet(viewsets.ModelViewSet):
 
     filter_backends = [SearchFilter]
     search_fields = ['first_name', 'last_name']
-
 
 
 class UserMeView(APIView):
