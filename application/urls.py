ed63575bd4ab10fa6c73b8ff3e99a468ff4fcb74
DmitrovicAleksa <aleksa.dmitrovic@vivifyideas.com>
2021-06-25 13:21:28 +0200

    Done

--- application/urls.py
+++ application/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from django.urls import path, include
+from django.urls import path, include,re_path
 from . import views
 from rest_framework.routers import DefaultRouter
 
--- application/views.py
+++ application/views.py
@@ -1,14 +1,17 @@
+from re import search
 from django.db.models import query
 from django.http import request
 from rest_framework import views
+import rest_framework
+from rest_framework.generics import ListAPIView
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import ToDo, User
 from .serializers import UserSerializer
 from rest_framework import mixins, viewsets
 from rest_framework import authentication, permissions
-from rest_framework.parsers import JSONParser
 from .permissions import AdminAccessPermission
+from rest_framework.filters import SearchFilter
 
 
 class HomeView(APIView):
@@ -17,11 +20,15 @@ class HomeView(APIView):
         return Response({"todos": todos})
 
 
+
 class UsersViewSet(viewsets.ModelViewSet):
 
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [AdminAccessPermission]
+
+    filter_backends = [SearchFilter]
+    search_fields = ['first_name', 'last_name']
 
 
 
